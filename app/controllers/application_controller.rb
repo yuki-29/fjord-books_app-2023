f0e4cd05c5ecc125a25af94a2bf4150bf4dfeb9c
yuki <y09425717@gmail.com>
2025-09-24 15:33:07 +0900

    ログイン時のリダイレクトを設定。deviseビューファイルの生成。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource_or_scope)
+    books_path
+  end
+
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
