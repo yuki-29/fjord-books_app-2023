66514c24dafd18cb52b49500ab66d7924a9845fd
yuki <y09425717@gmail.com>
2025-10-01 14:10:28 +0900

    ユーザー編集画面とDB保存を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,30 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :address, :self_introduction])
+  end
+
+  # アカウント編集後のリダイレクト先
+  def after_update_path_for(resource)
+    puts "====== after_update_path_forが呼ばれました！ ======"
+    edit_user_registration_path
+  end
 
   private
 
   # ログイン後のリダイレクト先
-  def after_sign_in_path_for(resource_or_scope)
+  def after_sign_in_path_for(resource)
     books_path
   end
 
   # ログアウト後のリダイレクト先
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(resource)
     new_user_session_path
   end
+
 end
---
+++ db/migrate/20250926060730_add_details_to_users.rb
@@ -0,0 +1,7 @@
+class AddDetailsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :postal_code, :string
+    add_column :users, :address, :string
+    add_column :users, :self_introduction, :text
+  end
+end
