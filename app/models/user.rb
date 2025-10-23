69ec65a5507efe6c0f0ce71dc5a77f2ca3037f01
yuki <y09425717@gmail.com>
2025-10-23 14:20:53 +0900

    ユーザー編集画面に画像アップロード機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [150, 150]
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
