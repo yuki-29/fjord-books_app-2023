2925928b4ad64f51e919aec17fb59e4de85aa059
yuki <y09425717@gmail.com>
2025-11-21 10:59:59 +0900

    日報の新規作成、詳細表示を実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,38 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
 
   def index
     @reports = Report.order(:id).page(params[:page])
   end
+
+  def show; end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+
+    respond_to do |format|
+      if @report.save
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
+        # format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        # format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:title, :body)
+  end
+
 end
