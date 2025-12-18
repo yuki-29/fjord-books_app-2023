ce7647a413aba491edbfe27a79aee234012201fb
yuki <y09425717@gmail.com>
2025-12-18 15:40:53 +0900

    reports_controllerファイルのコメントアウトを削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -23,10 +23,8 @@ class ReportsController < ApplicationController
     respond_to do |format|
       if @report.save
         format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-        # format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
-        # format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -35,10 +33,8 @@ class ReportsController < ApplicationController
     respond_to do |format|
       if @report.update(report_params)
         format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
-        # format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        # format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -48,7 +44,6 @@ class ReportsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
-      # format.json { head :no_content }
     end
   end
 
