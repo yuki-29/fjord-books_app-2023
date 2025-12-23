1c4063d7590c053df26f60638a5fe53cba7ae426
yuki <y09425717@gmail.com>
2025-12-23 14:52:18 +0900

    mentionsテーブルの作成、関連付けの設定

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,9 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :mentions
+  has_many :mentioning_report, through: :mentions
+  has_many :mentioned_report, through: :mentions
 
   validates :title, presence: true
   validates :content, presence: true
