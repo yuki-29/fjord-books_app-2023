6ab03c5122910b49a2630e9e711d0e062a7f2615
yuki <y09425717@gmail.com>
2025-12-29 10:29:33 +0900

    Reportモデルの関連付けでclass_nameを文字列に変更

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,8 +4,8 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
   # 1:多 レポートから、mentioning_report, mentioned_reportを紐づける。
-  has_many :active_mentions, class_name: :Mention, foreign_key: :mentioning_report_id, dependent: :destroy
-  has_many :passive_mentions, class_name: :Mention, foreign_key: :mentioned_report_id, dependent: :destroy
+  has_many :active_mentions, class_name: 'Mention', foreign_key: :mentioning_report_id, dependent: :destroy
+  has_many :passive_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy
   # 多:多
   # has_many :mentioning_reports, through: :active_mentions, source: :mentioned_report
   # has_many :mentioned_reports, through: :passive_mentions, source: :mentioning_report
